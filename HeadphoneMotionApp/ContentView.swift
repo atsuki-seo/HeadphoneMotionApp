427075e318f93ff572de914d3209ce720d5f91ee
atsuki.seo <atsuki.seo@mixi.co.jp>
2025-10-23 16:32:19 +0900

    Initial Commit

---
+++ HeadphoneMotionApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeadphoneMotionApp
+//
+//  Created by atsuki.seo on 2025/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeadphoneMotionApp/HeadphoneMotionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadphoneMotionAppApp.swift
+//  HeadphoneMotionApp
+//
+//  Created by atsuki.seo on 2025/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HeadphoneMotionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
